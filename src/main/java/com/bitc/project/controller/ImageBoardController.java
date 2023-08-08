385c39fc2460bdaad531bad58918ac84f12ef466
m1njunK <rla6085@naver.com>
2023-08-09 00:08:38 +0900

    web.xml 인코딩 필터 추가

--- src/main/java/com/bitc/project/controller/ImageBoardController.java
+++ src/main/java/com/bitc/project/controller/ImageBoardController.java
@@ -1,15 +1,20 @@
 package com.bitc.project.controller;
 
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.bitc.project.vo.ImageBoardVO;
 
@@ -17,7 +22,6 @@ import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequestMapping("/imageBoard")
-@RequiredArgsConstructor
 public class ImageBoardController {
 
 	@GetMapping("imgBoard_list")
@@ -27,7 +31,7 @@ public class ImageBoardController {
 	public void imgBoardForm() {}
 	
 	@PostMapping("create")
-	public String create(ImageBoardVO vo,Model model) {
+	public String create(ImageBoardVO vo,Model model) throws UnsupportedEncodingException {
 		System.out.println(vo);
 		List<ImageBoardVO> list = new ArrayList<>();
 		list.add(vo);
