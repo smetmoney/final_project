e6f711571ea413a7bca28976eb33ca22dd86bb90
m1njunK <rla6085@naver.com>
2023-08-09 16:03:11 +0900

    이미지게시판작업

--- src/main/java/com/bitc/project/dao/ImageBoardDAO.java
+++ src/main/java/com/bitc/project/dao/ImageBoardDAO.java
@@ -2,13 +2,17 @@ package com.bitc.project.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
 
 import com.bitc.project.vo.ImageBoardVO;
 
 public interface ImageBoardDAO {
 	
-	@Select("SELECT * FROM test_imageBoard")
+	@Select("SELECT * FROM test_imageBoard limit 0,6")
 	List<ImageBoardVO> imageBoardList()throws Exception;
+	
+	@Insert("INSERT INTO test_imageBoard VALUES(#{title},#{content},#{auth},now(),0,0,#{imageURL})")
+	int createImageBoard(ImageBoardVO vo);
 	
 }
--- src/main/java/com/bitc/project/service/ImageBoardServiceImpl.java
+++ src/main/java/com/bitc/project/service/ImageBoardServiceImpl.java
@@ -2,11 +2,11 @@ package com.bitc.project.service;
 
 import java.util.List;
 
-import org.springframework.stereotype.Service;
+import javax.servlet.ServletContext;
 
+import org.springframework.stereotype.Service;
 import com.bitc.project.dao.ImageBoardDAO;
 import com.bitc.project.vo.ImageBoardVO;
-
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -22,9 +22,8 @@ public class ImageBoardServiceImpl implements ImageBoardService {
 	}
 
 	@Override
-	public String createImageBoard() throws Exception {
-
-		return null;
+	public int createImageBoard(ImageBoardVO vo) throws Exception {
+		return dao.createImageBoard(vo);
 	}
-
+	
 }
