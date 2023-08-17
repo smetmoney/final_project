a50ba03521289d0f8ca2e9867a9ba8a924b47e8a
m1njunK <rla6085@naver.com>
2023-08-18 00:35:28 +0900

    결합

--- src/main/java/com/bitc/project/handler/ChattingHandler.java
+++ src/main/java/com/bitc/project/handler/ChattingHandler.java
@@ -1,13 +1,17 @@
 package com.bitc.project.handler;
 
-import org.json.JSONObject;
+// import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
-import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
+import lombok.extern.log4j.Log4j2;
 
 @Component
 @Log4j2
@@ -46,4 +50,28 @@ public class ChattingHandler extends TextWebSocketHandler {
         list.remove(session);
         // sessions.remove(session);
     }
+	/*    
+	// websocket 연결 성공 시
+	@Override 
+	public void afterConnectionEstablished(WebSocketSession session)throws Exception {
+		log.info("#ChattingHandler, afterConnectionEstablished");
+		sessionList.add(session);
+		log.info(session.getPrincipal().getName() + "님이 입장하셨습니다."); 
+	}
+	// websocket 메세지 수신 및 송신
+	@Override 
+	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+	log.info("#ChattingHandler, handleMessage"); log.info(session.getId() + ": "+ message);
+		for(WebSocketSession s : sessionList) { 
+			s.sendMessage(new TextMessage(session.getPrincipal().getName() + ":" + message.getPayload()));
+		} 
+	}
+	// websocket 연결 종료 시
+	@Override 
+	public void afterConnectionClosed(WebSocketSession session,	CloseStatus status) throws Exception {
+		log.info("#ChattingHandler, afterConnectionClosed");
+		sessionList.remove(session);
+		log.info(session.getPrincipal().getName() + "님이 퇴장하셨습니다."); 
+	} 
+	*/
 }
