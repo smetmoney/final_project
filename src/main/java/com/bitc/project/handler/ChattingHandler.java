eb1c6f1522c388ee975142ae4a12b5a4f66957e9
m1njunK <rla6085@naver.com>
2023-08-29 02:35:02 +0900

    채팅 기능 끝

--- src/main/java/com/bitc/project/handler/ChattingHandler.java
+++ src/main/java/com/bitc/project/handler/ChattingHandler.java
@@ -1,7 +1,6 @@
 package com.bitc.project.handler;
 
-// import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
-
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -13,72 +12,139 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
-import lombok.extern.log4j.Log4j;
-import lombok.extern.log4j.Log4j2;
+import lombok.Getter;
 
 @Component
+@Getter
 public class ChattingHandler extends TextWebSocketHandler {
-  
+
     private List<WebSocketSession> list = new ArrayList<>();
-    private Map<String, List<WebSocketSession>> room = new HashMap<>();
+    private List<Map<String, Object>> roomList = new ArrayList<>();
+    private int roomNum = 1;
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        
-    	String payload = message.getPayload();
-        String[] code = payload.split(":");  
-        
-        if(code[0].equals("ENTER")) {
-            String roomCode = code[1];
+        String payload = message.getPayload();
+        String[] code = payload.split(":");
+
+        if (code[0].equals("ENTER")) {
+            String roomNum = code[1];
             String userId = code[2];
-            
-            session.getAttributes().put("userId",userId);
-            
-            room.putIfAbsent(roomCode, new ArrayList<>());
-            List<WebSocketSession> roomSessions = room.get(roomCode);
-            roomSessions.add(session);
-            
-            for (WebSocketSession user : roomSessions) {
-            	if(user.isOpen()) {
-            		user.sendMessage(new TextMessage(userId+"님이 접속하였습니다!"));
-            	}
+
+            session.getAttributes().put("userId", userId);
+            session.getAttributes().put("roomNum", roomNum);
+
+            Map<String, Object> room = findMap(roomList, roomNum);
+            if (room != null) {
+                List<WebSocketSession> participants = (List<WebSocketSession>) room.getOrDefault("participant", new ArrayList<>());
+                if (participants.isEmpty()) {
+                    session.getAttributes().put("owner", userId);
+                    room.put("owner", userId);
+                }
+                participants.add(session);
+                room.put("participant", participants);
+                for (WebSocketSession user : participants) {
+                    if (user.isOpen()) {
+                        user.sendMessage(new TextMessage(userId + "님이 접속하였습니다!"));
+                    }
+                }
             }
-        }else {
-        	List<WebSocketSession> roomSessions = room.get(code[0]);
+        } else if (code[0].equals("GET_USERS")) {
+            Map<String, Object> room = findMap(roomList, code[1]);
+            updateUsersInRoom(room);
+        } else {
+            Map<String, Object> room = findMap(roomList, code[0]);
+            String userId = code[1];
+            List<WebSocketSession> roomSessions = (List<WebSocketSession>) room.get("participant");
             for (WebSocketSession user : roomSessions) {
-            	if(user.isOpen()) {
-            		user.sendMessage(new TextMessage(code[1]+":"+code[2]));
-            	}
+                if (user.isOpen()) {
+                    user.sendMessage(new TextMessage(userId + ":" + makeMassage(code)));
+                }
             }
         }
     }
 
-	@Override
+    @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         list.add(session);
+        
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-    	list.remove(session);
-    	List<WebSocketSession> list = null;
-    	String userId = (String) session.getAttributes().get("userId");
-        for (List<WebSocketSession> roomSessions : room.values()) {
-            if(roomSessions.contains(session)) {
-            	roomSessions.remove(session);
-            	list = roomSessions;
-            	break;
+        list.remove(session);
+
+        String userId = (String) session.getAttributes().get("userId");
+        String roomNum = (String) session.getAttributes().get("roomNum");
+
+        Map<String, Object> room = findMap(roomList, roomNum);
+        if (room != null) {
+            List<WebSocketSession> roomSessions = (List<WebSocketSession>) room.get("participant");
+            if (roomSessions.contains(session) && userId.equals(room.get("owner"))) {
+                roomSessions.remove(session);
+                if (!roomSessions.isEmpty()) {
+                    String newOwner = (String) roomSessions.get(0).getAttributes().get("userId");
+                    room.put("owner", newOwner);
+                    for (WebSocketSession user : roomSessions) {
+                        if (user.isOpen()) {
+                            user.sendMessage(new TextMessage("방장이 탈주하였습니다!! 새로운 방장은 " + newOwner + "입니다!"));
+                        }
+                    }
+                } else {
+                    roomList.remove(room);
+                }
+            } else {
+                roomSessions.remove(session);
+                for (WebSocketSession user : roomSessions) {
+                    if (user.isOpen()) {
+                        user.sendMessage(new TextMessage(userId + "님이 탈주하였습니다!"));
+                    }
+                }
             }
-        }
-        for(WebSocketSession s : list) {
-        	s.sendMessage(new TextMessage(userId+"님이 탈주하였습니다!"));
+            updateUsersInRoom(room);
         }
     }
-    
-    public Map<String, List<WebSocketSession>> getRoom() {
-		return room;
-	}
-	public void setRoom(Map<String, List<WebSocketSession>> room) {
-		this.room = room;
-	}
+
+    public Map<String, Object> creatRoom() {
+        Map<String, Object> createdRoom = new HashMap<>();
+        createdRoom.put("roomNum", roomNum);
+        roomList.add(createdRoom);
+        roomNum++;
+        return createdRoom;
+    }
+
+    public Map<String, Object> findMap(List<Map<String, Object>> list, String roomNum) {
+        int num = Integer.parseInt(roomNum);
+        for (Map<String, Object> map : list) {
+            if (map.containsValue(num)) {
+                return map;
+            }
+        }
+        return null;
+    }
+
+    public String makeMassage(String[] code) {
+        StringBuilder message = new StringBuilder();
+        for (int i = 2; i < code.length; i++) {
+            message.append(code[i]);
+        }
+        return message.toString();
+    }
+
+    private void updateUsersInRoom(Map<String, Object> room) throws IOException {
+        List<WebSocketSession> participants = (List<WebSocketSession>) room.get("participant");
+        List<String> userList = new ArrayList<>();
+
+        for (WebSocketSession participant : participants) {
+            userList.add((String) participant.getAttributes().get("userId"));
+        }
+
+        String userListStr = String.join(",", userList);
+
+        for (WebSocketSession participant : participants) {
+            if (participant.isOpen()) {
+                participant.sendMessage(new TextMessage("USERS:" + userListStr));
+            }
+        }
+    }
 }
--- src/main/webapp/WEB-INF/views/chat/chatRoom.jsp
+++ src/main/webapp/WEB-INF/views/chat/chatRoom.jsp
@@ -11,16 +11,17 @@
         </div>
         <c:choose>
             <c:when test="${!empty chatRoom}">
-            <div id="chatList" class="centered">
-                <c:forEach items="${chatRoom}" var="room">
-                    <div class="chatRoomBox">
-                        <a href="joinChat?roomNum=${room}">
-                            ${room}번방
-                        </a>
-                        <h4>${list.room.size}</h4>
-                    </div>
-                </c:forEach>
-                </div>
+	            <div id="chatList" class="centered">
+	                <c:forEach items="${chatRoom}" var="room">
+	                    <div class="chatRoomBox">
+	                        <a href="joinChat?roomNum=${room.roomNum}">
+	                            ${room.roomNum}번방
+	                        </a>
+	                        <h4>방장 : ${room.owner}</h4>
+	                        <h4>접속인원 : ${room.participant.size()}명</h4>
+	                    </div>
+	                </c:forEach>
+	            </div>
             </c:when>
             <c:otherwise>
                 <h1>아직 생성된 방이 없군요! 방을 만들어서 채팅을 나눠보세요!</h1>
@@ -30,10 +31,19 @@
         <button class="createButton" onclick='create()'>방만들기</button>
     </div>
 <script>
+	let id = '${userInfo}' == '';
 	function create(){
+		if(id){
+			alert('로그인 후 이용해 주세요!');
+			return;
+		}
 		location.href="/project/chat/create";
 	}
 	$(".chatRoomBox").on("click",function(){
+		if(id){
+			alert('로그인 후 이용해 주세요!');
+			return;		
+		}
 		location.href = $(this).find('a').attr('href');
 	});
 </script>
