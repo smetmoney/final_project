a8e10156c23d23aa9fcedb5ab1ea0e700c277a38
pangilK <129753181+pangilK@users.noreply.github.com>
2023-08-29 01:26:01 +0900

    챗봇 기능 넣는중

--- src/main/java/com/bitc/project/controller/HomeController.java
+++ src/main/java/com/bitc/project/controller/HomeController.java
@@ -1,31 +1,35 @@
 package com.bitc.project.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
 import java.util.Locale;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@PropertySources({
+	@PropertySource("classpath:prop/openai.properties")
+})
 @Controller
 public class HomeController {
-    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
     
+	@Value("${apikey}")
+	private String apiKey;
+	private static final String ENDPOINT = "https://api.openai.com/v1/chat/completions";
+	
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String home(Locale locale, Model model) {
-        logger.info("Welcome home! The client locale is {}.", locale);
-        
-        Date date = new Date();
-        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-        
-        String formattedDate = dateFormat.format(date);
-        
-        model.addAttribute("serverTime", formattedDate);
-        
         return "home";
+    }
+    
+    @GetMapping("/getKey")
+    @ResponseBody
+    public String getKey() {
+    	return apiKey+","+ENDPOINT;
     }
 }
