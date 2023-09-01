6e9fd30c1113a685747bdb49b90ff7cc34e77697
pangilK <129753181+pangilK@users.noreply.github.com>
2023-09-01 10:05:39 +0900

    Update nickcheck.js

--- src/main/webapp/resources/game/js/nickcheck.js
+++ src/main/webapp/resources/game/js/nickcheck.js
@@ -1,13 +1,14 @@
 const pattern =  /^[ㄱ-ㅎ가-힣a-zA-Z0-9]+$/;
+var nick;
 
 function nickCheck(){
   const inputField = document.getElementById("nickCheck");
-  const nick = inputField.value;
-  if(pattern.test(nick) && nick.length > 2){
+  if(pattern.test(inputField.value) && inputField.value.length > 2){
   	document.querySelector("#gameBox").style.display = 'block';
   	document.querySelector("#itemBox").style.display = 'block';
 	  document.querySelector("#start_1").remove();
-	  displayText(scene1);
+    nick = inputField.value;
+	  getScene();
   }else{
 	  alert('닉네임을 다시 확인해주시기 바랍니다.');
   }
